3931cc9d6fee18df335835417d8904280f5356bb
shumwe <bravinshumwe@gmail.com>
2022-03-06 01:41:59 +0300

    0014

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
-DEBUG = True
+DEBUG = False
 
 # Authentication handler for django/allauth
 AUTHENTICATION_BACKENDS = [
@@ -146,14 +146,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #cloudinary storage : handling imaeges in deployment
 
-"""
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'hqrwhofew',
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-"""
+
 
 
 # Default primary key field type
